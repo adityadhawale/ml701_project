fbbf92539744ddac898636f58fc2a9c63f0d58bf
Aditya Dhawale <adityadhawale6@gmail.com>
2019-03-08 11:58:50 -0500

    does feature scaling on data before training

--- phow_classifier.py
+++ phow_classifier.py
@@ -13,14 +13,29 @@ def main():
     parser.add_argument("--prefix", help='define the prefix of the file names')
     parser.add_argument("--classifier", help='what classifier should be trained:\n'
                         '\n0. logistic_regression\n'
-                        '1. gnb\n')
+                        '1. gnb\n'
+                        '2. multi_nb\n'
+                        '3. lin_svm\n')
     parser.add_argument('--pre_trained', default=False, type=bool)
     parser.add_argument('--save_model', default=False, type=bool)
 
     args = parser.parse_args()
 
+    hist = False
+    non_negative = False
     if args.prefix and args.classifier:
-        labeled_data = get_labeled_data(args.prefix)
+        if args.classifier == "gnb" or args.classifier == "multi_nb":
+            hist = True
+        labeled_data = get_labeled_data(args.prefix, hists=hist)
+
+        # if(not hist):
+        #     labeled_data = remove_degenerate_features(labeled_data)
+
+        if(args.classifier == "multi_nb"):
+            non_negative = True
+
+        labeled_data = pre_condition_data(
+            labeled_data, non_negative=non_negative)
         training_data, test_data =\
             split_data_into_test_and_training(
                 labeled_data)
@@ -32,10 +47,13 @@ def main():
             classifier_obj.fit(training_data)
             if(args.save_model):
                 classifier_obj.save_model(args.prefix)
-
         print("Training Score: {0}, Test Score: {1}".format(
             classifier_obj.get_score(training_data),
             classifier_obj.get_score(test_data)))
+
+        # print("Training Score: {0}, Test Score: {1}".format(
+        #     np.min(classifier_obj.classifier.predict_proba(training_data)),
+        #     np.min(classifier_obj.classifier.predict_proba(test_data))))
 
         # predict labels
         predicted_labels = classifier_obj.predict(test_data)

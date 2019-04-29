b503b781afa0cfce6a6d51c4d8751e369333930d
Ada Taylor <ada@idu.com>
2019-04-29 19:00:57 -0400

    Added options for importing augmented data either of all classes or a particular one

--- matlab_to_py_interface.py
+++ matlab_to_py_interface.py
@@ -30,16 +30,69 @@ def split_data_into_test_and_training(data, batch_size=15):
     return training_data, test_data
 
 
-def get_labeled_data(path_prefix, hists=False):
+# def get_labeled_data(path_prefix, hists=False):
+#     label_file = path_prefix + "gt_labels.mat"
+#     data_file = path_prefix + "psix.mat"
+#     if hists:
+#         data_file = path_prefix + "hists.mat"
+
+#     training_data = dict()
+#     training_data['X'] = np.array(get_matlab_matrix(data_file), dtype=np.float)
+#     training_data['labels'] =\
+#         np.array(get_matlab_matrix(label_file), dtype=np.int64)
+
+#     try:
+#         if training_data['labels'].shape[1]:
+#             training_data['labels'] = \
+#                 training_data['labels'].reshape(
+#                     training_data['labels'].shape[0], )
+#     except:
+#         pass
+
+#     assert training_data['X'].shape[0] == training_data['labels'].shape[0], \
+#         "Unequal number of data points and labels"
+
+#     return training_data
+
+def get_data_from_label(path_prefix, hists=False):
     label_file = path_prefix + "gt_labels.mat"
     data_file = path_prefix + "psix.mat"
     if hists:
         data_file = path_prefix + "hists.mat"
 
+    X = np.array(get_matlab_matrix(data_file), dtype=np.float)
+    labels = np.array(get_matlab_matrix(label_file), dtype=np.int64)
+
+    return X, labels
+
+
+def get_labeled_data(path_prefix, hists=False, aug_all=False, aug_class=-1):
+    # Get the vanilla data
     training_data = dict()
-    training_data['X'] = np.array(get_matlab_matrix(data_file), dtype=np.float)
-    training_data['labels'] =\
-        np.array(get_matlab_matrix(label_file), dtype=np.int64)
+    X, labels = get_data_from_label(path_prefix, hists)
+
+    aug_list = ['logs/baseline-20-l-', 'logs/baseline-20-r-', 'logs/baseline-40-l-', 'logs/baseline-40-r-', 'logs/zoom-.5-', 'logs/zoom-1.5-', 'logs/zoom-2-']
+
+    if aug_all:
+        for aug in aug_list:
+            aug_X, aug_label = get_data_from_label(aug, hists)
+            X = np.vstack((X, aug_X))
+            labels = np.vstack((labels, aug_label))
+            print(X.shape, labels.shape)
+            
+    elif aug_class > -1:
+        for aug in aug_list:
+            aug_X, aug_label = get_data_from_label(aug)
+            
+            for i in range(aug_label.shape[0]):
+                # if of target class, add its augmentations to the data set
+                if aug_label[i] is aug_class:
+                    print("Augmenting data for class " + i)
+                    X = np.vstack((X, aug_X[i]))
+                    labels = np.vstack((labels, aug_label[i]))
+
+    training_data['X'] = X
+    training_data['labels'] = labels
 
     try:
         if training_data['labels'].shape[1]:
@@ -53,7 +106,6 @@ def get_labeled_data(path_prefix, hists=False):
         "Unequal number of data points and labels"
 
     return training_data
-
 
 def split_training_into_train_and_cv(data, percentage_split=0.1, random=True):
     training = dict()
